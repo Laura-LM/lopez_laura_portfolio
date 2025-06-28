91aa4b3f9c8c474737296389cdfbc439b1ad0eff
llopezmaldonado <llopezmaldonado@academic.rrc.ca>
2025-06-28 18:50:27 -0500

    new images

--- src/components/HeroImage/HeroImage.stories.tsx
+++ src/components/HeroImage/HeroImage.stories.tsx
@@ -17,7 +17,7 @@ const Template = (args: any) => <HeroImage {...args} />;
 
 export const Default = Template.bind({}) as any;
 Default.args = {
-  src: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80',
+  src: 'https://images.unsplash.com/photo-1499002238440-d264edd596ec?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   alt: 'Nature',
   height: '300px',
   disabled: false,
--- src/components/Img/Img.stories.tsx
+++ src/components/Img/Img.stories.tsx
@@ -17,7 +17,7 @@ const Template = (args: any) => <Img {...args} />;
 
 export const Default = Template.bind({}) as any;
 Default.args = {
-  src: 'https://via.placeholder.com/300x200',
+  src: 'https://images.unsplash.com/photo-1569690681342-d74eb25436fd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   alt: 'Placeholder image',
   width: '300',
   height: '200',
